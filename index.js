c7f63f7bf985a226a2b07f4cd2d739e79be406bd
ritik <gupta.ritik2002@gmail.com>
2024-01-17 11:16:35 +0530

    first commit in gateway

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/shopping", proxy("http://localhost:8003")); //shopping
+app.use("/customer", proxy("http://localhost:8001")); //customer
+app.use("/", proxy("http://localhost:8002")); //products
+
+app.listen(8000, () => {
+  console.log("root is listening to the port 8000");
+});
